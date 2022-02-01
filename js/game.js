e5fb8847602d4b78df31ee93b5526ae9f727556c
Kim yeon su <rla5258@gmail.com>
2022-02-01 22:27:32 +0900

    api, Toastify 적용

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
-const SETTING_TIME = 5;
-let words02 = ["1", "2", "3"];
-let words = [
+const SETTING_TIME = 120;
+let words = [];
+let words02 = [
     "넌 신 같은 거 될 수도 없고, 되어서도 안 돼.",
     "끝과 시작의 밤에, 한 편의 이야기를 지어 비밀로 간직하라.",
     "상서로운 짐승과 선인이 살아나 이야기의 주인을 찾으리니.",
@@ -11,11 +11,15 @@ let words = [
     "비켜. 난 내 반려를 보러왔으니까.",
     "난 네가 수 천 번 날 바람 맞혀도 화 안나.",
     "가재나 게 같은 걸 별로 안 좋아하는 것뿐이야.",
+    "제가 당신을 얼마나 열렬히 흠모하고 사랑하는지를 말씀드리지 않을 수가 없습니다.",
+    "김자크 사랑합니다.",
+    "",
 ];
 let time;
 let isPlaying = false;
 let score = 0;
 
+const url = "https://random-word-api.herokuapp.com/word?number=100";
 const wordDisplay = document.querySelector(".word-display");
 const wordInput = document.querySelector(".word-input");
 const scoreDisplay = document.querySelector(".score");
@@ -27,6 +31,42 @@ const bar = document.querySelector(".progress__bar");
 const checkBtn = document.getElementById("check_btn");
 
 
+
+
+runToast = (text) => {
+    const option = {
+        text: text,
+        duration: 1000,
+        newWindow: true,
+        offset: {
+            x: 0, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: 120 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+        position: 'center',
+        backgroundColor: 'linear-gradient(45deg, #58cf8f, #69dc82 )',
+
+    }
+    Toastify(option).showToast()
+
+}
+runToast()
+
+const gerWords = () => {
+    axios
+        .get(url)
+        .then((res) => {
+            words = res.data.filter((word) => word.length < 8);
+            button.innerText = "Start";
+            stopBtn.disabled = true;
+            stopBtn.classList.remove("loading");
+            isPlaying = true;
+        })
+        .catch((err) => console.log(err));
+};
+const init = () => {
+    time = SETTING_TIME;
+    gerWords();
+};
 
 //게임초기화
 
@@ -41,16 +81,12 @@ function timeReset() {
     stopBtn.classList.add("loading");
 }
 
-
-
 function reset() {
     time = 120;
     score = 0;
     timeDisplay.innerText = time;
     button.innerText = "Playing Now";
     btn.disabled = false;
-    stopBtn.disabled = true;
-    stopBtn.classList.remove("loading");
 
     if (time <= time + 1) {
         bar.classList.add("animation");
@@ -61,9 +97,8 @@ function reset() {
     wordDisplay.innerText = words[randomIndex];
 }
 //제한시간
-time = 120;
-const countDown = () => {
 
+const countDown = () => {
     if (time > 0) {
         time--;
 
@@ -82,19 +117,21 @@ const countDown = () => {
 //게임시작
 
 const checkMach = () => {
-
-
     let inputValue = wordInput.value.toLowerCase();
     let wordDisplayTxt = wordDisplay.innerText.toLowerCase();
 
     if (inputValue === wordDisplayTxt) {
         score += 10;
+        runToast("Great!!")
+
 
         wordInput.value = "";
         const randomIndex = Math.floor(Math.random() * words.length);
 
         wordDisplay.innerText = words[randomIndex];
     }
+
+
 
     scoreDisplay.innerText = score;
 
@@ -106,6 +143,9 @@ const checkMach = () => {
 
 //event handler
 function run() {
+    if (isPlaying === false) {
+        return;
+    }
     reset();
     timeInterval = setInterval(countDown, 1000);
     checkMach();
@@ -113,7 +153,6 @@ function run() {
 
     btn.disabled = true;
     stopBtn.disabled = false;
-
 }
 
 function stopTime() {
@@ -122,3 +161,7 @@ function stopTime() {
         timeReset();
     }
 }
+
+//getting Ready
+
+init();
